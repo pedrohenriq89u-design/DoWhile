c7ae171090ebb044dba8908ef24dc302f618d270
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-05-13 16:47:42 -0300

    first commit

---
+++ src/Ex02.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ex02 {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        int senha;
+
+        do {
+            IO.println("Digite a senha:");
+            senha = sc.nextInt();
+            if (senha != 1234) {
+                IO.println("Acesso Negado");
+            }
+        } while (senha != 1234);
+        IO.println("Acesso Permitido.");
+    }
+}
